1714dfec3015eb6e3f115b2beae0c86d00522155
William S. Moses <gh@wsmoses.com>
2021-05-23 16:54:11 -0400

    Add numerical verification

--- DG/cuda/verify.jl
+++ DG/cuda/verify.jl
@@ -198,11 +198,8 @@ function main()
     ntrials = 10
     DFloat = Float32
 
-    points = [20_000, 40_000, 80_000, 160_000, 240_000]
-    points = (1:12) * 20_000
-    # points = 240_000
+    nelem = 20_000
 
-    for nelem in points
 
     rnd = MersenneTwister(0)
 
@@ -229,51 +226,14 @@ function main()
 
     # @cuda threads=threads blocks=nelem volumerhs_good!(rhs′, Q, vgeo, DFloat(grav), D, nelem)
 
-    kernel = @cuda launch=false volumerhs!(rhs, Q, vgeo, DFloat(grav), D, nelem)
-    kernel(rhs, Q, vgeo, DFloat(grav), D, nelem; threads=threads, blocks=nelem)
-
-    # @show rhs′ ≈ rhs
-
-    CUDA.@profile for _ = 1:ntrials
-        res = CUDA.@timed kernel(rhs, Q, vgeo, DFloat(grav), D, nelem; threads=threads, blocks=nelem)
-        push!(results, (kind=:primal, nelems=nelem, time=res.time))
-    end
-
-    if !PROFILING
-        open("vrhs.ll", "w") do io
-            CUDA.@device_code_llvm io=io dump_module=true raw=true debuginfo=:none @cuda threads=threads blocks=nelem volumerhs!(rhs, Q, vgeo, DFloat(grav), D, nelem)
-        end
-    end
-
     @info "Starting Enzyme run"
 
-    if !PROFILING
-        # Enzyme.API.EnzymeSetCLBool(:EnzymePreopt, false)
-        Enzyme.API.EnzymeSetCLBool(:EnzymePrintPerf, true)
-        Enzyme.API.EnzymeSetCLBool(:EnzymePrint, true)
-        # Enzyme.API.EnzymeSetCLBool(:EnzymePrintActivity, true)
-    end
-    Enzyme.API.EnzymeSetCLBool(:EnzymeRegisterReduce, false)
+    Enzyme.API.EnzymeSetCLBool(:EnzymeRegisterReduce, true)
     # Enzyme.API.EnzymeSetCLString(:EnzymeBCPath, "/home/wmoses/git/Enzyme/enzyme/bclib")
 
-    kernel = @cuda launch=false dvolumerhs_const!(rhs, Q, vgeo, DFloat(grav), D, nelem)
-    kernel(rhs, Q, vgeo, DFloat(grav), D, nelem; threads=threads, blocks=nelem)
-
-    CUDA.@profile for _ = 1:ntrials
-        res = CUDA.@timed kernel(rhs, Q, vgeo, DFloat(grav), D, nelem; threads=threads, blocks=nelem)
-        push!(results, (kind=:all_const, nelems=nelem, time=res.time))
-    end
-
-    if !PROFILING
-        open("const_dvrhs.ll", "w") do io
-            CUDA.@device_code_llvm io=io dump_module=true raw=true debuginfo=:none @cuda threads=threads blocks=nelem dvolumerhs_const!(rhs, Q, vgeo, DFloat(grav), D, nelem)
-        end
-    end
-
-    @info "Full Enzyme run"
-
     drhs = similar(rhs)
-    drhs .= 1
+    drhs .= 0
+    drhs[1, 1, 1, 2, 1:1] .= 1
 
     dvgeo = similar(vgeo)
     dvgeo .= 0
@@ -287,28 +247,14 @@ function main()
     kernel = @cuda launch=false dvolumerhs!(rhs, drhs, Q, dQ, vgeo, dvgeo, DFloat(grav), D, dD, nelem)
     kernel(rhs, drhs, Q, dQ, vgeo, dvgeo, DFloat(grav), D, dD, nelem; threads=threads, blocks=nelem)
 
-    CUDA.@profile for _ = 1:ntrials
-        res = CUDA.@timed kernel(rhs, drhs, Q, dQ, vgeo, dvgeo, DFloat(grav), D, dD, nelem; threads=threads, blocks=nelem)
-        push!(results, (kind=:all_dub, nelems=nelem, time=res.time))
-    end
+    o1 = rhs[1, 1, 1, 2, 1:1]
+    Q[1] += 1e-4
+    rhs .= 0
 
-    if !PROFILING
-        open("dvrhs.ll", "w") do io
-            CUDA.@device_code_llvm io=io dump_module=true raw=true debuginfo=:none @cuda threads=threads blocks=nelem dvolumerhs!(rhs, drhs, Q, dQ, vgeo, dvgeo, DFloat(grav), D, dD, nelem)
-        end
-    end
-
-    CUDA.unsafe_free!(dD)
-    CUDA.unsafe_free!(D)
-    CUDA.unsafe_free!(dQ)
-    CUDA.unsafe_free!(Q)
-    CUDA.unsafe_free!(dvgeo)
-    CUDA.unsafe_free!(vgeo)
-    CUDA.unsafe_free!(drhs)
-    CUDA.unsafe_free!(rhs)
-    @show results
-    end
-    CSV.write("profile.csv", results)
+    kernel = @cuda launch=false volumerhs!(rhs, Q, vgeo, DFloat(grav), D, nelem)
+    kernel(rhs, Q, vgeo, DFloat(grav), D, nelem; threads=threads, blocks=nelem)
+    o2 = rhs[1, 1, 1, 2, 1:1]
+    @show dQ[1], (o2-o1) / 1e-4
 end
 
 main()
